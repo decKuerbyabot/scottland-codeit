beb83f4057afd65d1d4ecdb444e9d6419278f08f
Issac Nicks <1975354252wangqi@gmail.com>
2022-09-16 22:37:25 +0800

    asdfsadf

--- App.py
+++ App.py
@@ -11,7 +11,7 @@ handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+# logger.setLevel(logging.INFO)
 
 @app.route('/', methods=['GET'])
 def default_route():
