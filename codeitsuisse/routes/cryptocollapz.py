acf3f473cf94ac45bf7ca017bde93b38a16c3814
Issac Nicks <1975354252wangqi@gmail.com>
2022-09-17 11:46:07 +0800

    asdfaf

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -6,31 +6,60 @@
 # logger = logging.getLogger(__name__)
 
 
+from cgitb import lookup
+
+
 def solve_cryptocollapz(lol):
+    lookup_table=dict()
     sol=[]
     for l in lol:
         l_sol=[]
         for n in l:
-            l_sol.append(int(find_max(n)))
+            if n not in lookup_table.keys():
+                maxval=find_max(n, lookup_table)
+                l_sol.append(int(maxval))
+                lookup_table[n]=maxval
+            else:
+                l_sol.append(lookup_table[n])
         sol.append(l_sol)
     return sol
 
 
-def find_max(x):
+def find_max(x, lt):
     if x==4:
        return x
     elif x%2==0:
-        return max(find_max(x/2), x)
+        halfval=int(x/2)
+        if halfval in lt.keys():
+            maxhalf=lt[halfval]
+        else:
+            maxhalf=find_max(halfval, lt)
+            lt[halfval]=maxhalf
+        return max(maxhalf, x)
     else:
-        return max(find_max(3*x+1), x)
+        halfval=3*x+1
+        if halfval in lt.keys():
+            maxhalf=lt[halfval]
+        else:
+            maxhalf=find_max(halfval, lt)
+            lt[halfval]=maxhalf
+        return max(maxhalf, x)
+
 
 if __name__=="__main__":
     input=[
+
         [ 1, 2, 3, 4, 5 ],
-        [ 6, 7, 8, 9, 10 ],
+        # [ 10000, 10000, 10000, 10000, 10000],
     ]
+    input_1=[]
+    # for i in range(100000):
+        # input_1.append([ 901294, 182913, 289123, 281942, 882132])
+    
     output=[
         [ 4, 4, 16, 4, 16 ],
         [ 16, 52, 8, 52, 16 ],
     ]
-    print(solve(input))
+    print(solve_cryptocollapz(input))
+    # print(solve_cryptocollapz(input_1))
+
